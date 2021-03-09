557833487697c82a3df5617a8b308a9e9b76639d
LuisTaborda <luisgustavo.taborda@gmail.com>
2021-03-09 20:05:26 -0300

    filter with name poke

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="app">
+  <div id="app" class="column is-half is-offset-one-quarter">
     <img src ="./assets/pokemon-logo.png" id="logo"/>
-    <div class="column is-half is-offset-one-quarter"> 
-        <div v-for="(poke,index) in pokemons" :key="index">
+    <h4 class="is-size-4">Pokedex</h4>
+    <input class="input is-rounded" type="text" placeholder="Find pokemon with name" v-model="finded"/>
+    <button class="button is-fullwidth is-link" id="findBtn" @click="find">Finder</button>
+    <div> 
+        <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
           <Pokemon :name="poke.name" :url="poke.url" :num="index + 1"/>
         </div>
     </div>
@@ -14,22 +17,35 @@
 <script>
 import axios from 'axios';
 import Pokemon from './components/pokemon'
-export default {
+  export default {
   name: 'App',
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      filteredPokemons:[],
+      finded:''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
       this.pokemons = res.data.results;
-      console.log("Get list of the pokemons")
-
+      this.filteredPokemons = res.data.results;
     })
   },
   components:{
     Pokemon
+  },
+  methods:{
+    find: function(){
+      this.filteredPokemons = this.pokemons
+      if(this.finded == '' || this.finded == ' '){
+        this.filteredPokemons = this.pokemons
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name.indexOf(this.finded.toLowerCase()) != -1);
+      }
+    }
+  },
+  computed:{
   }
 }
 </script>
@@ -44,8 +60,8 @@ export default {
   margin-top: 60px;
 
 }
-#logo{
-  width: 600px;
+#findBtn{
+  margin-top: 10px;
 }
 
 </style>
