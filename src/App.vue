06cd28c3827c8fc62214b183018b7d312a31642f
LuisTaborda <luisgustavo.taborda@gmail.com>
2021-03-08 21:14:28 -0300

    using axios with vue js, consuming api pokeapi.co

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,21 @@
 </template>
 
 <script>
-
-//import axios from 'axios';
+import axios from 'axios';
 export default {
   name: 'App',
-  
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      this.pokemons = res.data.results;
+      console.log("Get list of the pokemons")
+
+    })
+  }
 }
 </script>
 
